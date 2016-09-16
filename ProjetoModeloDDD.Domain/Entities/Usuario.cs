52dfe3076f31c2c7f469318f608c125bbc8f8d8b
stanleyalves <stanleyalv3s@gmail.com>
2016-09-16 14:24:20 -0300

    Alteraçãoi

--- ProjetoModeloDDD.Domain/Entities/Usuario.cs
+++ ProjetoModeloDDD.Domain/Entities/Usuario.cs
@@ -9,7 +9,7 @@ namespace ProjetoModeloDDD.Domain.Entities
     public class Usuario
     {
 
-        public int IdUsuario { get; set; }
+        public int UsuarioId { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
